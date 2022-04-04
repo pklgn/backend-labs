7b8ce4b1268c8998dab466f91f19346fcfbb52d0
pklgn <petechka_petrov_201@mail.ru>
2022-04-04 23:56:09 +0300

    [lab2-scrumBoard] add BoardCard

--- ScrumBoard/ScrumBoard/BoardCard.cs
+++ ScrumBoard/ScrumBoard/BoardCard.cs
@@ -11,6 +11,30 @@ namespace ScrumBoard
 {
     class BoardCard
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
 
+        public BoardCard(string name, string description = "", int priority = 1)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Priority = priority;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            this.Description = description;
+        }
+
+        public void ChangeName(string name)
+        {
+            this.Name = name;
+        }
+
+        public void ChangePriority(int priority)
+        {
+            this.Priority = priority;
+        }
     }
 }
