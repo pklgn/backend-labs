7d3b453809494a321b374aed5d4f797a6932bf50
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-05-17 18:58:04 +0300

    [lab2] add basics for console app

---
+++ ScrumBoard/ScrumBoardConsole/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScrumBoard;
+
+namespace ScrumBoardConsole
+{
+    class Program
+    {
+        private enum ProgramCommand
+        {
+            CreateBoard,
+            CreateColumn,
+            RenameColumn,
+            RemoveColumn,
+            AddCard,
+            RemoveCard,
+            PrintBoard,
+            Help,
+            Skip,
+            Exit,
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+
+        private ProgramCommand ReadCommand()
+        {
+            string commandString = Console.ReadLine().Trim();
+
+            if (commandString == "create_board")
+            {
+                return ProgramCommand.CreateBoard;
+            }
+            else if (commandString == "create_column")
+            {
+                return ProgramCommand.CreateColumn;
+            }
+            else if (commandString == "rename_column")
+            {
+                return ProgramCommand.RenameColumn;
+            }
+            else if (commandString == "remove_column")
+            {
+                return ProgramCommand.RemoveColumn;
+            }
+            else if (commandString == "add_card")
+            {
+                return ProgramCommand.AddCard;
+            }
+            else if (commandString == "remove_card")
+            {
+                return ProgramCommand.RemoveCard;
+            }
+            else if (commandString == "print_board")
+            {
+                return ProgramCommand.PrintBoard;
+            }
+            else if (commandString == "help")
+            {
+                return ProgramCommand.Help;
+            }
+            else if (commandString == "exit")
+            {
+                return ProgramCommand.Exit;
+            }
+
+            return ProgramCommand.Skip;
+        }
+
+        
+    }
+}
