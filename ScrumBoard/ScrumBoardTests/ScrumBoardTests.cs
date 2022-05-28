760101bba2474bafa41ae8300deba57438b06ab9
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-05-28 22:47:20 +0300

    [lab3] add tests for column and board

--- ScrumBoard/ScrumBoardTests/ScrumBoardTests.cs
+++ ScrumBoard/ScrumBoardTests/ScrumBoardTests.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using ScrumBoard;
+using System.Collections.Generic;
 
 namespace ScrumBoardTests
 {
@@ -24,6 +25,66 @@ namespace ScrumBoardTests
             Assert.Equal(_testBoardTitle, board.Title);
         }
 
-        
+        [Fact]
+        public void CreateColumn_ArgumentIsColumnName_AddColumnToBoard()
+        {
+            //Arrange
+            Board board = new Board(_testBoardTitle);
+            BoardColumn column = new BoardColumn(_testColumnTitle);
+
+            //Act
+            bool isSuccessful = board.AppendColumn(column);
+
+            //Assert
+            Assert.True(isSuccessful);
+            Assert.Single(board.GetBoardColumns());
+            Assert.Equal(_testColumnTitle, board.GetBoardColumns()[0].Title);
+        }
+
+        [Fact]
+        public void CreateColumn_ArgumentHasExistingColumnName()
+        {
+            //Arrange
+            Board board = new Board(_testBoardTitle);
+            BoardColumn firstColumn = new BoardColumn(_testColumnTitle);
+            BoardColumn secondColumn = new BoardColumn(_testColumnTitle);
+
+            //Act
+            bool isFirstSuccessful = board.AppendColumn(firstColumn);
+            bool isSecondSuccessful = board.AppendColumn(secondColumn);
+
+            //Assert
+            Assert.True(isFirstSuccessful);
+            Assert.True(isSecondSuccessful);
+            Assert.Equal(2, board.GetBoardColumns().Count);
+            Assert.Collection(board.GetBoardColumns(),
+                firstColumn => Assert.Equal(_testColumnTitle, firstColumn.Title),
+                secondColumn => Assert.Equal(_testColumnTitle, secondColumn.Title)
+            );
+        }
+
+        [Fact]
+        public void AppendColumn_AddMoreThanLimitColumnAmount()
+        {
+            //Arrange
+            Board board = new Board(_testBoardTitle);
+            List<BoardColumn> boardColumns = new List<BoardColumn>();
+
+            for (int i = 0; i < Board.MAX_COLUMN_AMOUNT + 1; ++i)
+            {
+                boardColumns.Add(new BoardColumn("column" + i));
+            }
+
+            //Act
+            bool isSuccessful = true;
+            foreach (BoardColumn column in boardColumns)
+            {
+                isSuccessful = board.AppendColumn(column);
+            }
+
+            //Assert
+            Assert.Equal(Board.MAX_COLUMN_AMOUNT, board.GetBoardColumns().Count);
+            Assert.False(isSuccessful);
+        }
     }
 }
