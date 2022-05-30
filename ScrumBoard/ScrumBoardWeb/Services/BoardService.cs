1e8f1b9afa02fb9c23e4c09525a2d3a3c26dd414
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-05-31 00:26:59 +0300

    [lab4] add Services

---
+++ ScrumBoard/ScrumBoardWeb/Services/BoardService.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using ScrumBoardWeb.Repository;
+
+namespace ScrumBoardWeb.Services;
+
+public class BoardService : IBoardService
+{
+    private readonly ScrumBoardRepository _repository;
+
+    public BoardService(ScrumBoardRepository repository) => _repository = repository;
+
+    public void CreateBoard()
+    {
+
+
+        return;
+    }
+    public void CreateBoardCard() => throw new NotImplementedException();
+    public void CreateBoardColumn() => throw new NotImplementedException();
+    public void RemoveBoard() => throw new NotImplementedException();
+}
---
+++ ScrumBoard/ScrumBoardWeb/Services/IBoardService.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace ScrumBoardWeb.Services;
+
+public interface IBoardService
+{
+    public void CreateBoard();
+    public void RemoveBoard();
+
+    public void CreateBoardColumn();
+
+    public void CreateBoardCard();
+
+}
