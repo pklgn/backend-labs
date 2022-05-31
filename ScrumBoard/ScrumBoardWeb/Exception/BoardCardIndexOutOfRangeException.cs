5244ebc8369b965b7f3caaa4b487a1483373805c
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-06-01 00:14:43 +0300

    [lab4] add out of range exceptions

---
+++ ScrumBoard/ScrumBoardWeb/Exception/BoardCardIndexOutOfRangeException.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace ScrumBoardWeb.Exception;
+
+public class BoardCardIndexOutOfRangeException : System.Exception
+{
+    public BoardCardIndexOutOfRangeException() : base("Board card index is out of range")
+    {
+    }
+}
---
+++ ScrumBoard/ScrumBoardWeb/Exception/BoardColumnIndexOutOfRangeException.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace ScrumBoardWeb.Exception;
+
+public class BoardColumnIndexOutOfRangeException : System.Exception
+{
+    public BoardColumnIndexOutOfRangeException() : base("Board column index is out of range")
+    {
+    }
+}
