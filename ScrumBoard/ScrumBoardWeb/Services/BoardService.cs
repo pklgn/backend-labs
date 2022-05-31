f8522e55ac0e188199cc91ddf5640cdbee4bc9ae
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-06-01 00:15:40 +0300

    [lab4] add board service

--- ScrumBoard/ScrumBoardWeb/Services/BoardService.cs
+++ ScrumBoard/ScrumBoardWeb/Services/BoardService.cs
@@ -1,23 +1,74 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using ScrumBoard;
+using ScrumBoardAPI.DTO;
+using ScrumBoardWeb.DTO;
 using ScrumBoardWeb.Repository;
 
 namespace ScrumBoardWeb.Services;
 
 public class BoardService : IBoardService
 {
-    private readonly ScrumBoardRepository _repository;
+    private readonly IScrumBoardRepository _repository;
 
-    public BoardService(ScrumBoardRepository repository) => _repository = repository;
+    public BoardService(IScrumBoardRepository repository) => _repository = repository;
 
-    public void CreateBoard()
+    public void CreateBoard(string title)
     {
+        CreateBoardDTO board = new CreateBoardDTO(title);
 
+        _repository.CreateBoard(board);
 
         return;
     }
-    public void CreateBoardCard() => throw new NotImplementedException();
-    public void CreateBoardColumn() => throw new NotImplementedException();
-    public void RemoveBoard() => throw new NotImplementedException();
+
+    public void RemoveBoard(int index)
+    {
+        _repository.RemoveBoard(index);
+
+        return;
+    }
+
+    public List<BoardDTO> GetBoards()
+    {
+        return _repository.GetBoardsDTO();
+    }
+
+    public BoardDTO GetBoard(int index)
+    {
+        return _repository.GetBoardDTO(index);
+    }
+
+    public void CreateBoardCard(int boardIndex, int columnIndex, string name, string description, string priority)
+    {
+        BoardCardDTO card = new BoardCardDTO(name, description, priority);
+        
+        _repository.AddCard(boardIndex, columnIndex, card);
+
+        return;
+    }
+
+    public void RemoveBoardCard(int boardIndex, int columnIndex, int cardIndex)
+    {
+        _repository.RemoveCard(boardIndex, columnIndex, cardIndex);
+
+        return;
+    }
+
+    public void CreateBoardColumn(int boardIndex, string name)
+    {
+        CreateBoardColumnDTO column = new CreateBoardColumnDTO(name);
+
+        _repository.CreateColumn(boardIndex, column);
+
+        return;
+    }
+
+    public void RemoveBoardColumn(int boardIndex, int columnIndex)
+    {
+        _repository.RemoveColumn(boardIndex, columnIndex);
+
+        return;
+    }
 }
--- ScrumBoard/ScrumBoardWeb/Services/IBoardService.cs
+++ ScrumBoard/ScrumBoardWeb/Services/IBoardService.cs
@@ -1,15 +1,25 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using ScrumBoardWeb.DTO;
+
 namespace ScrumBoardWeb.Services;
 
 public interface IBoardService
 {
-    public void CreateBoard();
-    public void RemoveBoard();
+    public void CreateBoard(string title);
 
-    public void CreateBoardColumn();
+    public void RemoveBoard(int index);
 
-    public void CreateBoardCard();
+    public List<BoardDTO> GetBoards();
 
+    public BoardDTO GetBoard(int index);
+
+    public void CreateBoardColumn(int boardIndex, string name);
+
+    public void RemoveBoardColumn(int boardIndex, int columnIndex);
+
+    public void CreateBoardCard(int boardIndex, int columnIndex, string name, string description, string priority);
+
+    public void RemoveBoardCard(int boardIndex, int columnIndex, int cardIndex);
 }
