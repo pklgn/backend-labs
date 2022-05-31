eaa4730eb06b1acff225aa105f63c70474fd3969
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-06-01 00:15:21 +0300

    [lab4] add interface and update repository methods

---
+++ ScrumBoard/ScrumBoardWeb/Repository/IScrumBoardRepository.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using ScrumBoard;
+using ScrumBoardAPI.DTO;
+using ScrumBoardWeb.DTO;
+
+namespace ScrumBoardWeb.Repository;
+
+public interface IScrumBoardRepository
+{
+    public void CreateBoard(CreateBoardDTO boardDTO);
+
+    public void RemoveBoard(int index);
+
+    public BoardDTO GetBoardDTO(int index);
+
+    public List<BoardDTO> GetBoardsDTO();
+
+    public List<Board> GetBoards();
+
+    public void CreateColumn(int boardIndex, CreateBoardColumnDTO column);
+
+    public void RemoveColumn(int boardIndex, int columnIndex);
+
+    public void AddCard(int boardIndex, int columnIndex, BoardCardDTO card);
+
+    public void RemoveCard(int boardIndex, int columnIndex, int cardIndex);
+}
--- ScrumBoard/ScrumBoardWeb/Repository/ScrumBoardRepository.cs
+++ ScrumBoard/ScrumBoardWeb/Repository/ScrumBoardRepository.cs
@@ -1,20 +1,23 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Linq;
 using Microsoft.Extensions.Caching.Memory;
 using ScrumBoard;
+using ScrumBoardAPI.DTO;
 using ScrumBoardWeb.DTO;
+using ScrumBoardWeb.Exception;
 
 namespace ScrumBoardWeb.Repository;
 
-public class ScrumBoardRepository
+public class ScrumBoardRepository : IScrumBoardRepository
 {
     private readonly IMemoryCache _memoryCache;
     private const string _memoryCacheBoardKey = "boards";
 
     public ScrumBoardRepository(IMemoryCache memoryCache) => _memoryCache = memoryCache;
 
-    public void CreateBoard(BoardDTO boardDTO)
+    public void CreateBoard(CreateBoardDTO boardDTO)
     {
         List<Board> boards = GetBoards();
 
@@ -23,25 +26,159 @@ public class ScrumBoardRepository
         _memoryCache.Set(_memoryCacheBoardKey, boards);
     }
 
-    public BoardDTO GetBoard(int index)
+    public void RemoveBoard(int index)
     {
         List<Board> boards = GetBoards();
 
-        if (boards.Count == 0 || index < 0 || index >= boards.Count)
+        try
         {
-            
+            boards.RemoveAt(index);
         }
+        catch
+        {
+            throw new BoardIndexOutOfRangeException();
+        }
+
+        _memoryCache.Set(_memoryCacheBoardKey, boards);
     }
 
-    private List<Board> GetBoards()
+    public BoardDTO GetBoardDTO(int index)
+    {
+        List<BoardDTO> boards = GetBoardsDTO();
+
+        if (boards.Count == 0 || index < 0 || index >= boards.Count)
+        {
+            throw new BoardIndexOutOfRangeException();
+        }
+
+        return boards[index];
+    }
+
+    public List<BoardDTO> GetBoardsDTO()
+    {
+        List<Board>? boards = _memoryCache.Get<List<Board>>(_memoryCacheBoardKey);
+
+        List<BoardDTO> boardsDTO = new List<BoardDTO>();
+
+        if (boards == null)
+        {
+            return boardsDTO;
+        }
+
+        foreach (var board in boards)
+        {
+            List<BoardColumnDTO> boardColumnsDTO = new List<BoardColumnDTO>();
+            foreach (var column in board.GetBoardColumns())
+            {
+                boardColumnsDTO.Add(new BoardColumnDTO(column));
+            }
+            boardsDTO.Add(new BoardDTO(board.Title, boardColumnsDTO));
+        }
+
+        return boardsDTO;
+    }
+
+    public List<Board> GetBoards()
     {
         List<Board>? boards = _memoryCache.Get<List<Board>>(_memoryCacheBoardKey);
 
         if (boards == null)
         {
-            boards = new List<Board>();
+            return new List<Board>();
         }
 
         return boards;
+    }
+
+    public void CreateColumn(int boardIndex, CreateBoardColumnDTO column)
+    {
+        List<Board> boards = GetBoards();
+
+        if (boards.Count == 0 || boardIndex < 0 || boardIndex >= boards.Count)
+        {
+            throw new BoardIndexOutOfRangeException();
+        }
+
+        boards[boardIndex].AppendColumn(new BoardColumn(column.Name));
+
+        _memoryCache.Set(_memoryCacheBoardKey, boards);
+    }
+
+    public void RemoveColumn(int boardIndex, int columnIndex)
+    {
+        List<Board> boards = GetBoards();
+
+        if (boards.Count == 0 || boardIndex < 0 || boardIndex >= boards.Count)
+        {
+            throw new BoardIndexOutOfRangeException();
+        }
+
+        Board board = boards[boardIndex];
+
+        List<BoardColumn> columns = board.GetBoardColumns();
+
+        if (columns.Count == 0 || boardIndex < 0 || boardIndex >= columns.Count)
+        {
+            throw new BoardColumnIndexOutOfRangeException();
+        }
+
+        columns.RemoveAt(columnIndex);
+
+        _memoryCache.Set(_memoryCacheBoardKey, boards);
+    }
+
+    public void AddCard(int boardIndex, int columnIndex, BoardCardDTO card)
+    {
+        List<Board> boards = GetBoards();
+
+        if (boards.Count == 0 || boardIndex < 0 || boardIndex >= boards.Count)
+        {
+            throw new BoardIndexOutOfRangeException();
+        }
+
+        Board board = boards[boardIndex];
+
+        List<BoardColumn> columns = board.GetBoardColumns();
+
+        if (columns.Count == 0 || boardIndex < 0 || boardIndex >= columns.Count)
+        {
+            throw new BoardColumnIndexOutOfRangeException();
+        }
+
+        columns[columnIndex].AppendCard(new BoardCard(card.Name, card.Description, BoardCard.GetPriorityTypeFromString(card.Priority)));
+
+        _memoryCache.Set(_memoryCacheBoardKey, boards);
+    }
+
+    public void RemoveCard(int boardIndex, int columnIndex, int cardIndex)
+    {
+        List<Board> boards = GetBoards();
+
+        if (boards.Count == 0 || boardIndex < 0 || boardIndex >= boards.Count)
+        {
+            throw new BoardIndexOutOfRangeException();
+        }
+
+        Board board = boards[boardIndex];
+
+        List<BoardColumn> columns = board.GetBoardColumns();
+
+        if (columns.Count == 0 || boardIndex < 0 || boardIndex >= columns.Count)
+        {
+            throw new BoardColumnIndexOutOfRangeException();
+        }
+
+        BoardColumn column = columns[columnIndex];
+
+        try
+        {
+            column.GetBoardCards().RemoveAt(cardIndex);
+        }
+        catch
+        {
+            throw new BoardCardIndexOutOfRangeException();
+        }
+
+        _memoryCache.Set(_memoryCacheBoardKey, boards);
     }
 }
