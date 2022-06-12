fe4611973436a41241097304e4601c7bb780a72d
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-06-12 18:55:49 +0300

    [lab5] add infrastructure for db compability

---
+++ ScrumBoard/ScrumBoardInfrastructure/ScrumBoardInfrastructure.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using ScrumBoard;
+using ScrumBoardInfrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScrumBoardInfrastructure;
+
+public class ScrumBoardContext : DbContext
+{
+    public DbSet<BoardModel> Boards => Set<BoardModel>();
+    public DbSet<BoardColumnModel> BoardColumns => Set<BoardColumnModel>();
+    public DbSet<BoardCardModel> BoardCards => Set<BoardCardModel>();
+
+    public ScrumBoardContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql("server=localhost;user=root;password=!safe_Password1?;database=lab5;",
+                new MySqlServerVersion(new Version(5, 7, 29)));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Ignore<Board>();
+        modelBuilder.Ignore<BoardColumn>();
+        modelBuilder.Ignore<BoardCard>();
+
+        modelBuilder.Entity<BoardCardModel>().HasKey(c => c.CardId);
+
+    }
+}
